bea68c8efe81ab14359cac363fe44978bd325f4e
kffl <pawel@kuffel.io>
2020-04-25 14:21:10 +0200

    sufficient tasks

--- tasks-sufficient/sufficient.go
+++ tasks-sufficient/sufficient.go
@@ -1,7 +1,17 @@
 package main
 
+//feel free to define additional functions as needed
+
 func palindrome(str string) bool {
 	return false
+}
+
+func wordcount(str string) string {
+	return "a"
+}
+
+func rotate(numbers []int) []int {
+	return []int{4, 1, 2, 3}
 }
 
 func fib(nums []int) int {
